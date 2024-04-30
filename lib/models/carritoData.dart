fd891e48855071165e14d787329c247efa15aeab
Dani <unan1515@gmail.com>
2024-04-30 10:04:57 +0200

    Crear modelo de los carritos

---
+++ lib/models/carritoData.dart
@@ -0,0 +1,11 @@
+class Carrito {
+  // Properties
+  String nombre;
+  String productos;
+  double precio;
+
+  // Constructor
+  Carrito({required this.nombre, required this.productos, required this.precio});
+
+
+}
