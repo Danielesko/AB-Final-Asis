5ec5e82c75057f09762b85524342391db65ee8f7
Dani <unan1515@gmail.com>
2024-05-28 18:21:55 +0200

    Front end casi terminado pantalla principal

--- lib/Screen/principal.dart
+++ lib/Screen/principal.dart
@@ -20,40 +20,63 @@ class _SesionState extends ConsumerState<Sesion> {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Carritos'),
+        title: Row(
+          children: [
+            Image.asset(
+              'assets/images/logo.png',
+              width: 30,
+              height: 30,
+            ),
+            const SizedBox(width: 10),
+            const Text('Carritos'),
+          ],
+        ),
+        backgroundColor: Colors.grey,
       ),
-      body: ListView.builder(
-          itemBuilder: (ctx, index) => Card(
-                child: ListTile(
-                  title: Text(
-                    listaCarritos[index].nombre,
-                    style: const TextStyle(
-                        fontSize: 19, fontWeight: FontWeight.bold),
+      backgroundColor: Colors.grey,
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, 
+          childAspectRatio: 1.0, 
+        ),
+        itemBuilder: (ctx, index) => Container(
+          margin: const EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            color: Colors.orange,
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(.0),
+          ),
+          child: ListTile(
+            title: Text(
+              listaCarritos[index].nombre,
+              style: const TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
+            ),
+            trailing: Wrap(
+              children: [
+                IconButton(
+                  onPressed: () => delete(listaCarritos[index].id),
+                  icon: const Icon(
+                    Icons.delete,
+                    color: Colors.black,
                   ),
-                  trailing: Wrap(
-                    children: [
-                      IconButton(
-                        onPressed: () => delete(listaCarritos[index].id),
-                        icon: const Icon(
-                          Icons.delete,
-                          color: Colors.red,
-                        ),
-                      ),
-                      IconButton(
-                        onPressed: () => edita(context, listaCarritos[index]),
-                        icon: const Icon(
-                          Icons.edit,
-                          color: Colors.red,
-                        ),
-                      ),
-                    ],
-                  ),
-                  onTap: () => mostrarCarrito(context, listaCarritos[index]),
                 ),
-              ),
-          itemCount: listaCarritos.length),
+                IconButton(
+                  onPressed: () => edita(context, listaCarritos[index]),
+                  icon: const Icon(
+                    Icons.edit,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+            onTap: () => mostrarCarrito(context, listaCarritos[index]),
+          ),
+        ),
+        itemCount: listaCarritos.length,
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => add(context),
+        backgroundColor: Colors.orange,
         child: const Icon(Icons.add),
       ),
     );
@@ -73,8 +96,8 @@ class _SesionState extends ConsumerState<Sesion> {
     ref.read(carritosProvider.notifier).delete(id);
   }
 
-  void mostrarCarrito (BuildContext context, Carrito carrito){
-    Navigator.of(context).push(MaterialPageRoute(
-        builder: (ctx) => carritoScreen(carrito: carrito)));
+  void mostrarCarrito(BuildContext context, Carrito carrito) {
+    Navigator.of(context).push(
+        MaterialPageRoute(builder: (ctx) => carritoScreen(carrito: carrito)));
   }
 }
